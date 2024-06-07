9985cfe1ee75e188e92e93b7083f0084d958411e
Rahul Chandna <Rahul.chandna2001@gmail.com>
2024-06-06 22:35:54 -0400

    Add Exit functionality in the app and also add some test cases.

--- Calculator.Console/Prompts.cs
+++ Calculator.Console/Prompts.cs
@@ -9,6 +9,8 @@ public class Prompts{
         Console.WriteLine("1. Add");
         Console.WriteLine("2. Subtract");
         Console.WriteLine("3. Multiply");
+        Console.WriteLine("4. Divide");
+        Console.WriteLine("5. Exit");
     }
 
 }
